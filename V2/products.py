fa3242c4626bbf810e27d8458b4f08fd5271209c
SrDjKiller <69609695+SrDjKiller@users.noreply.github.com>
2022-04-05 23:51:36 +0200

    V2 - Updated

---
+++ V2/products.py
@@ -0,0 +1,122 @@
+products = [
+    {
+        'Codigo':10,
+        'Producto':'Doritos TexMex',
+        'Precio':1.30
+    },
+    {
+        'Codigo':11,
+        'Producto':'Ruffles Jamon',
+        'Precio':1.30
+    },
+    {
+        'Codigo':12,
+        'Producto':'Pringles',
+        'Precio':1.40
+    },
+    {
+        'Codigo':13,
+        'Producto':'Lays Campesinas',
+        'Precio':1.30
+    },
+    {
+        'Codigo':20,
+        'Producto':'Croissant de Chocolate',
+        'Precio':1.20
+    },
+    {
+        'Codigo':21,
+        'Producto':'Palitos de semillas',
+        'Precio':0.90
+    },
+    {
+        'Codigo':22,
+        'Producto':'Pan de Pipas',
+        'Precio':1.00
+    },
+    {
+        'Codigo':23,
+        'Producto':'Ositos Haribo',
+        'Precio':0.70
+    },
+    {
+        'Codigo':30,
+        'Producto':'Galletas Oreo',
+        'Precio':1.20
+    },
+    {
+        'Codigo':31,
+        'Producto':'KitKat',
+        'Precio':1.10
+    },
+    {
+        'Codigo':32,
+        'Producto':'KinderBueno Blanco',
+        'Precio':1.35
+    },
+    {
+        'Codigo':33,
+        'Producto':'Lacasitos',
+        'Precio':0.80
+    },
+    {
+        'Codigo':40,
+        'Producto':'Conguitos',
+        'Precio':0.90
+    },
+    {
+        'Codigo':41,
+        'Producto':'Galletas Chips Ahoy mini',
+        'Precio':1.10
+    },
+    {
+        'Codigo':42,
+        'Producto':'Galletas Prinicipe',
+        'Precio':1.20
+    },
+    {
+        'Codigo':43,
+        'Producto':'Mikados',
+        'Precio':1.50
+    },
+    {
+        'Codigo':50,
+        'Producto':'Lata Cocacola',
+        'Precio':1.20
+    },
+    {
+        'Codigo':51,
+        'Producto':'Lata Aquarius Limon',
+        'Precio':1.20
+    },
+    {
+        'Codigo':52,
+        'Producto':'Lata Fanta Naranja',
+        'Precio':1.20
+    },
+    {
+        'Codigo':53,
+        'Producto':'Cacaolat 200ml',
+        'Precio':1.35
+    },
+    {
+        'Codigo':60,
+        'Producto':'Agua Bezoya 1L',
+        'Precio':1.00
+    },
+    {
+        'Codigo':61,
+        'Producto':'Agua Bezoya 1L',
+        'Precio':1.00
+    },
+    {
+        'Codigo':62,
+        'Producto':'Agua Bezoya 1L',
+        'Precio':1.00
+    },
+    {
+        'Codigo':63,
+        'Producto':'Agua Bezoya 1L',
+        'Precio':1.00
+    }
+]
---
+++ V2/vending_machineV2.py
@@ -0,0 +1,47 @@
+import time
+import products as p
+
+i = 0
+dinerot = 0
+cambio = 0
+seguir = False
+while seguir == False:
+      print("##..:Productos:..##")
+      for j in p.products:
+            print(f"Producto: {j['Producto']} - Precio: {j['Precio']} - Codigo: {j['Codigo']}")
+
+      seleccionP = int(input("Introduzca el número de código del artículo que desea obtener: "))
+      for j in p.products:
+            if seleccionP == j['Codigo']:
+                  producto = j
+      if producto == '':
+            print('CODIGO INVALIDO')
+      else:
+            print(f"Estupendo, {producto['Producto']} le costará {producto['Precio']}€")
+
+      while dinerot < producto['Precio']:
+            dinero = float(input("Introduce Cantidad, solo monedas de 0.50€, 1€ o 2€: "))
+            if (dinero == 0.5 or dinero == 1 or dinero == 2):
+                  dinerot = dinerot + dinero
+            else:
+                  print("Moneda Incorrecta o Inexistente")
+      
+      print("Expendiendo producto... Porfavor Espere")
+
+      time.sleep(2)
+
+      if dinerot > producto['Precio']:
+            cambio = dinerot - producto['Precio']
+            print(f"Gracias por comprar, aquí está su {producto['Producto']}")
+            time.sleep(2)
+            print(f"Su cambio es: {round(cambio,2)}€")
+      else:
+            print(f"Gracias por comprar aquí está su {producto['Producto']}")
+
+      time.sleep(3)
+
+      cont = input("Para salir de la máquina introduzca s y para seguir comprando pulse cualquier tecla y presione enter:  ")
+      if cont == 's' or cont == 'S':
+            seguir = False
+      else:
+            seguir = True
